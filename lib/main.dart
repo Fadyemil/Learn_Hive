07c65ba9ce9a8793750626568da213212c7e2647
fady emil <fady46t45f@gmail.com>
2024-07-13 22:41:16 +0300

    make Hive open and close

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/sockets/src/sockets_html.dart';
 import 'package:hive/hive.dart';
 import 'package:learn_hive/screen/my_home_page.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
@@ -9,10 +10,20 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() {
+    super.dispose();
+    Hive.box('School').close();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +32,21 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: FutureBuilder(
+        future: Hive.openBox('School'),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionStatus.connected) {
+            return const MyHomePage();
+          } else if (snapshot.hasError) {
+            print(snapshot.error.toString());
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      ),
     );
   }
 }
